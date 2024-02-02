9248e7bbe4ac7bc1e3fda3e28beec512d67b458d
felsantoss <felipesantossf@outlook.com>
2024-02-02 16:21:13 -0300

    Criação da Calculadora

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Somar()
+{
+    Console.WriteLine("Método Somar\n");
+    Console.WriteLine("Insira o primeiro valor:");
+    double firstValue = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Insira o segundo valor:");
+    double secondValue = Convert.ToDouble(Console.ReadLine());
+
+    double result = firstValue + secondValue;
+
+    Console.WriteLine($"{firstValue} + {secondValue} = {result}");
+}
+
+static void Subtratir()
+{
+    Console.WriteLine("Método Subtratir\n");
+
+    Console.WriteLine("Insira o primeiro valor:");
+    double firstValue = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Insira o segundo valor:");
+    double secondValue = Convert.ToDouble(Console.ReadLine());
+
+    double result = firstValue - secondValue;
+
+    Console.WriteLine($"{firstValue} - {secondValue} = {result}");
+}
+
+static void Multiplicar()
+{
+    Console.WriteLine("Método Multiplicar\n");
+
+    Console.WriteLine("Insira o primeiro valor:");
+    double firstValue = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Insira o segundo valor:");
+    double secondValue = Convert.ToDouble(Console.ReadLine());
+
+    double result = firstValue * secondValue;
+
+    Console.WriteLine($"{firstValue} * {secondValue} = {result}");
+}
+
+static void Dividir()
+{
+    Console.WriteLine("Método Dividir\n");
+
+    Console.WriteLine("Insira o primeiro valor:");
+    double firstValue = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Insira o segundo valor:");
+    double secondValue = Convert.ToDouble(Console.ReadLine());
+
+    double result = firstValue / secondValue;
+
+    Console.WriteLine($"{firstValue} / {secondValue} = {result}");
+}
+
+bool restart = true;
+
+while (restart)
+{
+    Console.WriteLine("Qual operação deseja realizar?\n");
+
+    Console.WriteLine("Digite * para multiplicar, / para dividir, + para somar e - para subtrair.");
+    string operation = Console.ReadLine();
+
+    switch (operation)
+{
+    case "*": 
+    Multiplicar();
+    break;
+
+    case "/":
+    Dividir();
+    break;
+
+    case "+":
+    Somar();
+    break;
+
+    case "-":
+    Subtratir();
+    break;
+
+    default: 
+    Console.WriteLine("Operação não encontrada");
+    break;
+}
+
+    Console.WriteLine("Deseja reiniciar o programa? s/n");
+    string response = Console.ReadLine();
+
+    restart = response.ToLower() == "s";
+}
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52f8cb6a4c8f4dda5a8872c2574be192e7da5ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632d3f4819470270c206bb12027bc55715fe779f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
