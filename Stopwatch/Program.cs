0b5fd59e9b461b5449c4ebee4f71fd0927d59a4d
felsantoss <felipesantossf@outlook.com>
2024-02-05 18:15:16 -0300

    Teste novo projeto

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Start()
+{
+    int time = 10;
+    int currentTime = 0;
+
+    while(currentTime != time)
+    {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        
+    }
+}
+
+
+Start();
