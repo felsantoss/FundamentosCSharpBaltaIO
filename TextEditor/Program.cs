8f9c081d3f19ef64c7f4222f2f64016f0f4de30a
felsantoss <felipesantossf@outlook.com>
2024-02-09 07:41:46 -0300

    Adicionando funcionalidade de Abrir, Editar e Salvar

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+static void Menu() {
+    Console.Clear();
+
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+
+    short option = short.Parse(Console.ReadLine());
+
+    switch(option)
+    {
+        case 0:
+        System.Environment.Exit(0);
+        break;
+
+        case 1:
+        Abrir();
+        break;
+
+        case 2:
+        Editar();
+        break;
+
+        default:
+        Menu();
+        break;
+    }
+}
+
+static void Abrir() {}
+
+static void Editar()
+{
+    Console.Clear();
+
+    Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+    Console.WriteLine("-------------------");
+
+    string texto = "";
+
+    do
+    {
+        texto += Console.ReadLine();
+        texto += Environment.NewLine;
+    }
+    while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Console.Write(texto);
+}
+
+Menu();
