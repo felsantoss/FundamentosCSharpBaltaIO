7e1fb65ae029b5b23cc8c2086e57c008fd52be08
felsantoss <felipesantossf@outlook.com>
2024-02-10 12:26:12 -0300

    Guid (Global Unique Identifier) valor se gerado randomicamente é quaaase impossível de se repetir

---
+++ String/Guids/Guids.md
@@ -0,0 +1,2 @@
+## Guid (Global Unique Identifier)
+### É um valor que se gerado randomicamente, irá gerar um identificador (quase) único.
---
+++ String/Guids/Program.cs
@@ -0,0 +1,6 @@
+﻿var id = Guid.NewGuid(); // gerando um novo Guid
+
+id = new Guid("b0b4e983-e854-4033-8ed3-01ad5488f317");
+
+Console.WriteLine(id);
+Console.WriteLine(id.ToString().Substring(0, 8)); // Pegando os 8 primeiros caracteres do guid 
