0f2d48dffec7a3dac889822e7cf1024170d2600a
felsantoss <felipesantossf@outlook.com>
2024-02-14 07:03:42 -0300

    String StartsWith e EndsWith

---
+++ String/StartsWithEndsWith/Program.cs
@@ -0,0 +1,7 @@
+﻿string texto = "Este texto é apenas um teste";
+
+Console.WriteLine(texto.StartsWith("Este")); // true
+Console.WriteLine(texto.StartsWith("este", StringComparison.OrdinalIgnoreCase)); // ignora case sensitive - true
+
+Console.WriteLine(texto.EndsWith("teste")); // true
+Console.WriteLine(texto.EndsWith("Teste")); // false
