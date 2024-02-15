e12dd4caaefababbdcfaea2001a472e5ab617443
felsantoss <felipesantossf@outlook.com>
2024-02-15 07:56:15 -0300

    Criando datas, data atual, data formatada e padrão universal

--- Datas/Program.cs
+++ Datas/Program.cs
@@ -1,4 +1,22 @@
 ﻿// Data atual
-var data = DateTime.Now;
+var dataAtual = DateTime.Now;
 
-Console.WriteLine(data);
+// Especificando uma data
+var data = new DateTime(2024, 02, 15, 07, 12, 30); 
+
+// Data formatada
+var dataFormatada = String.Format("{0:yyyy/MM/dd hh:mm:ss z}", dataAtual);
+Console.WriteLine(dataFormatada);
+
+var dataPadrao = String.Format("{0:u}", dataAtual);
+Console.WriteLine(dataPadrao);
+
+/*
+Console.WriteLine(dataAtual);
+Console.WriteLine(data.DayOfYear);
+Console.WriteLine(data.DayOfWeek); */
+
+// Adicionando dia, mês e ano
+Console.WriteLine(dataAtual.AddDays(12));
+Console.WriteLine(dataAtual.AddMonths(1));
+Console.WriteLine(dataAtual.AddYears(1));
