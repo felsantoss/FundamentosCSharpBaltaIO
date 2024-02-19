9603f616fd06d73fe582831386214daad3848d36
felsantoss <felipesantossf@outlook.com>
2024-02-19 07:29:52 -0300

    Moedas, convertendo para uma cultura especifica, arredondando usando o método Math.

---
+++ Moedas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+decimal valor = 25.50m;
+
+// Convertendo o valor da moeda informando uma cultura especifica
+Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+// Arredonda o número 
+Console.WriteLine(Math.Round(valor));
+
+// Arredonda para cima
+Console.WriteLine(Math.Ceiling(valor));
+
+// Arredonda para baixo
+Console.WriteLine(Math.Floor(valor));
