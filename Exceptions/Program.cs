3eec27ae56516f3a4b48ba54398a4062c3b05ce6
felsantoss <felipesantossf@outlook.com>
2024-02-23 07:36:19 -0300

    Módulo Exceptions

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -14,10 +14,32 @@ catch(IndexOutOfRangeException ex)
     Console.WriteLine(ex.Message);
     Console.WriteLine("Item excede a lista atual");
 }
+catch(ArgumentNullException ex)
+{
+    Console.WriteLine(ex.InnerException);
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Falha ao cadastrar");
+}
+
 // Exception para trativa de forma genérica
 catch(Exception ex)
 {
     Console.WriteLine("Algo deu errado!");
     Console.WriteLine(ex.InnerException);
     Console.WriteLine(ex.Message);
+} 
+// Sempre será executado
+finally
+{
+    Console.WriteLine("Fim do programa.");
+}
+
+Cadastrar(""); 
+
+static void Cadastrar(string texto)
+{
+    if (string.IsNullOrEmpty(texto))
+    {
+        throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
+    }
 }
