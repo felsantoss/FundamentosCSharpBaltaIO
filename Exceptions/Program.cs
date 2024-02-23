d3d4d9866a528a9b4b6a5136656ca656c18dee77
felsantoss <felipesantossf@outlook.com>
2024-02-23 07:04:35 -0300

    Try catch

---
+++ Exceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿var arr = new int[3];
+
+try 
+{
+    for ( int i = 0; i  < 10; i ++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Algo deu errado!");
+    Console.WriteLine(ex.InnerException);
+    Console.WriteLine(ex.Message);
+}
